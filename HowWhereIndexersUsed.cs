a17fc3be24948221572ba6c1209136112f357655
Marcus Canty <42560051+mcanty711@users.noreply.github.com>
2021-11-27 21:39:04 -0500

    Update HowWhereIndexersUsed.cs
    These two files show how and where indexers are used. The first file stores data in a session-state variable and retrieves the data as either an index or as an integral indexer or a string indexer. The second file retrieves data from a SQL table and writes it to a Web Form. using ADO.NET Core.

--- HowWhereIndexersUsed.cs
+++ HowWhereIndexersUsed.cs
@@ -20,3 +20,35 @@ namespace HowWhereIndexersUsed
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson64_HowWhereIndexersUsed
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblEmployee", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Response.Write("Id = " + rdr[0].ToString() + " ");
+                    Response.Write("LastName = " + rdr["LastName"].ToString());
+                    Response.Write("<br/>");
+                }
+            }
+        }
+    }
+}
